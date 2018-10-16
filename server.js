24d899832d8d26a2c0f23ca9a720839d998e01cc
Luke <ldpelegr@mtu.edu>
2018-10-16 17:43:41 -0400

    edited signup page, added server.js node file

---
+++ server.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var connect = require('connect');
+var express = require('express');
+
+var fs = require('fs');
+
+var app = express();
+console.log("Server is now online.\n");
+
+app.use(express.static('~/Documents/CollegeStuff/CS3141/trailmap'));
+
+// Route for everything else.
+app.get('*', function(request, response){
+    response.send('Error 404: Not found');
+});
+
+app.get("/home", function (request, response) {
+    response.writeHead(goodStatus);
+});
+
+app.listen(port);
