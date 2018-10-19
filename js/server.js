ec643466c36c61950ae8a6b3edc75a500f6f58ea
Michael Munoz <mmunoz779@outlook.com>
2018-10-18 21:13:59 -0400

    began improving server functionality and structure to allow for easy user navigation in addition to adjusting organization of files

--- js/server.js
+++ js/server.js
@@ -1,23 +1,46 @@
-var http = require('http');
-var connect = require('connect');
-var express = require('express');
+// Configure server constants
 const goodStatus = 200;
 const port = 8080;
 
-var fs = require('fs');
+// Configure express and routing
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Require routing directories
+var index = require('../routes/index');
+var api = require('../routes/api');
 
 var app = express();
-console.log("Server is now online.\n");
 
-app.use(express.static('C:\\Users\\mmuno\\WebstormProjects\\trail-map'));
+var directoryArray = __dirname.split('\\');
+var projectDirectory = "";
 
-// Route for everything else.
-app.get('*', function(request, response){
-    response.send('Error 404: Not found');
+for (var i = 0; i < directoryArray.length - 1; i++) {
+    projectDirectory += directoryArray[i] + '\\';
+}
+
+//View engine
+app.set('views', path.join(projectDirectory, 'client\\views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+// Configure static folder
+app.use(express.static(path.join(projectDirectory, 'client')));
+
+// Handle parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Handle routing
+app.use('/', index);
+app.use('/api', api);
+
+app.use("*", function (req, res) {
+    res.header(200);
+    res.send("Error 404: page not found");
 });
 
-app.get("/html", function (request, response) {
-    response.writeHead(goodStatus);
+app.listen(port, function () {
+    console.log("Server is now online on port " + port);
 });
-
-app.listen(port);
