c73d6c093ae67c069f5e7b9d97660bb9630e1445
Michael Munoz <mmunoz779@outlook.com>
2018-10-24 14:58:30 -0400

    added test json payload and loads in the map endpoint from the api

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 router.get('/api', function (req, res, next) {
     var content = fs.readFileSync('client/JSON/loops.json');
     console.log('API was accessed');
-    res.json(content);
+    res.json(JSON.parse(content));
 });
 
 module.exports = router;
