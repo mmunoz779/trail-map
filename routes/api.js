fd55d081b35219aecb4e9c4739da551a0f1f725c
Michael Munoz <mmunoz779@outlook.com>
2018-10-29 15:54:11 -0400

    Fixed date logging and post for route API, testing slack github notifications

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 router.use('/api', function (req, res, next) {
     // Add middleware to handle time logging
-    console.log('API was accessed at ' + Date.now().toLocaleString());
+    console.log('API was accessed at ' + new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''));
     next();
 });
 
@@ -15,6 +15,21 @@ router.get('/api/routes', function (req, res, next) {
 
 router.get('/api', function (req, res, next) {
     res.send("Available endpoints:\r\nroutes: returns JSON containing possible routes");
+});
+
+router.post("/api/routes", function (req, res, next) {
+    var content = fs.readFileSync('client/JSON/loops.json');
+
+    var data = JSON.parse(content);
+    data.push({
+            name: "",
+            distance: "",
+            imagePath: ""
+        }
+    );
+    console.log(data);
+    res.send("Post request received");
+    //fs.writeFileSync('client/JSON/loops.json', data);
 });
 
 module.exports = router;
