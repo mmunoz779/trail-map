e984616b3bf79e4e041d0cfdbdbbcd10d405036d
Michael Munoz <mmunoz779@outlook.com>
2018-11-02 14:56:50 -0400

    Added basic framework for Oauth2 support

--- js/server.js
+++ js/server.js
@@ -6,6 +6,22 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+
+/* Get a connection to the mySql database */
+const mySqlConnection = require('./dbHelpers/mySqlWrapper');
+
+/* The bearerTokenDBHelper handles all of the database operations
+relating to saving and retrieving oAuth2 bearer tokens */
+const bearerTokensDBHelper =
+    require('./dbHelpers/bearerTokensDBHelper')
+    (mySqlConnection);
+
+/* The userDBHelper handles all of the database operations relating
+to users such as registering and retrieving them */
+const userDBHelper = require('./dbHelpers/userDBHelper')
+(mySqlConnection);
+
+
 // Require routing directories
 var index = require('../routes/index');
 var api = require('../routes/api');
@@ -29,6 +45,51 @@ for (var i = 0; i < directoryArray.length - 1; i++) {
 app.set('views', path.join(projectDirectory, OSName != "Windows" ? 'client/views' : 'client\\views')); //sets directory for either windows or mac
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
+
+
+/* We require the node-oauth2-server library */
+const oAuth2Server = require('node-oauth2-server');
+
+/* Here we instantiate the model we just made and inject the dbHelpers we use in it */
+const oAuthModel =
+    require('./authorization/accessTokenModel')
+    (userDBHelper,
+        bearerTokensDBHelper);
+
+/* Now we instantiate the oAuth2Server and pass in an object which tells
+the the password library that we're using the password  grant type and
+give it the model we just required. */
+app.oauth = oAuth2Server({
+    model: oAuthModel,
+    grants: ['password'],
+    debug: true
+});
+
+
+/* Here we require the authRoutesMethods object from the module
+ that we just made */
+const authRoutesMethods =
+    require('./authorization/authRoutesMethods')
+    (userDBHelper);
+
+/* Now we instantiate the authRouter module and inject all
+of its dependencies. */
+const authRouter =
+    require('./authorization/authRouter')
+    (express.Router(),
+        app,
+        authRoutesMethods);
+
+/* Here we asign the authRouter as middleware in the express app.
+ By doing this all request sent to routes that start with /auth
+ will be handled by this router*/
+app.use('/auth', authRouter);
+
+/* Setup the oAuth error handling */
+app.use(app.oauth.errorHandler());
+
+
+app.use(app.oauth.errorHandler());
 
 // Configure static folder
 app.use(express.static(path.join(projectDirectory, 'client')));
